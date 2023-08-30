ac29c3764026e9f2c4d71c1f42332be31303dd78
Hanieh Kiaee <hanieh.k@780.ir>
2023-08-30 11:50:14 +0330

    Adding Entity Type configurations

---
+++ Infrastructure/Data/Config/BookConfig.cs
@@ -0,0 +1,25 @@
+﻿using Common.Constants;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasMany(book => book.Chapters)
+                   .WithOne()
+                   .HasForeignKey(chapter=> chapter.BookId)
+                   .IsRequired()
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(book => book.Title).HasColumnType(DbConst.Type.NVarchar_200);
+            builder.Property(book => book.Published_Date).HasColumnType(DbConst.Type.varchar_8);
+
+            builder.ToTable(DbConst.Table.Book, DbConst.Schema.Library);
+            builder.HasQueryFilter(book => !book.IsDeleted);
+        }
+    }
+}
---
+++ Infrastructure/Data/Config/ChapterConfig.cs
@@ -0,0 +1,19 @@
+﻿using Common.Constants;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ChapterConfig : IEntityTypeConfiguration<Chapter>
+    {
+        public void Configure(EntityTypeBuilder<Chapter> builder)
+        {
+            builder.Property(chapter => chapter.Title).HasColumnType(DbConst.Type.NVarchar_200);
+            //builder.Property(chapter => chapter.Text).HasColumnType("");
+
+            builder.ToTable(DbConst.Table.Chapter, DbConst.Schema.Library);
+            builder.HasQueryFilter(chapter => !chapter.IsDeleted);
+        }
+    }
+}
