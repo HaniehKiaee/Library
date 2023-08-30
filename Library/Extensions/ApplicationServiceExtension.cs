256bcbf294ef326f641eff54a5d13eac161aaeab
Hanieh Kiaee <hanieh.k@780.ir>
2023-08-30 11:55:31 +0330

    Register services

---
+++ Library/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Library.Mapping;
+
+namespace Library.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositoryServices();
+            services.AddSingleton(Assembly.GetExecutingAssembly());
+
+            //Add mapper
+            var MapperCfg = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new BookProfile());
+                mc.AddProfile(new ChapterProfile());
+            });
+            IMapper Mapper = MapperCfg.CreateMapper();
+            services.AddSingleton(Mapper);
+
+            //Add Context
+            var conn = configuration.GetConnectionString("Default");
+            services.AddDbContext<LibraryContext>(builder => builder.UseSqlServer(conn));
+
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddSwaggerGen(options =>
+            {
+                options.CustomSchemaIds(type => type.ToString());
+            });
+        }
+    }
+}
---
+++ Library/Extensions/RepositoryExtension.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Data.Repositories.Contracts;
+using Infrastructure.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Extensions
+{
+    public static class RepositoryExtension
+    {
+        public static void AddRepositoryServices(this IServiceCollection services)
+        {
+            services.AddTransient<IBookRepository, BookRepository>();
+            services.AddTransient<IChapterRepository, ChapterRepository>();
+            services.AddTransient<ILogErrorRepository, LogErrorRepository>();
+            services.AddTransient<ILogInformationRepository, LogInformationRepository>();
+        }
+    }
+}
