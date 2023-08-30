27ffeebc9c58c6d96cc06cda8785ef663fb49b7f
Hanieh Kiaee <hanieh.k@780.ir>
2023-08-30 12:00:04 +0330

    Mapper

---
+++ Library/Mapping/BookProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain;
+using Infrastructure.Data.Service.Paging;
+using Library.Models.BookDtos;
+using System.Collections.Generic;
+
+namespace Library.Mapping
+{
+    public class BookProfile: Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<Book, BookSummaryDto>();
+            CreateMap<CreateBookDto, Book>();
+            CreateMap<CustomGetBookDto, PagingParam>();
+        }
+    }
+}
---
+++ Library/Mapping/ChapterProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain;
+using Infrastructure.Data.Service.Paging;
+using Library.Models.ChapterDtos;
+
+namespace Library.Mapping
+{
+    public class ChapterProfile : Profile
+    {
+        public ChapterProfile()
+        {
+            CreateMap<Chapter, ChapterSummaryDto>();
+            CreateMap<CreateChapterDto, Chapter>();
+            CreateMap<CustomGetChapterDto, PagingParam>();
+        }
+    }
+}
