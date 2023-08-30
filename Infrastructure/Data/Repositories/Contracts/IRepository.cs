1d39071d51ad1f68b5472154c210346ca943e395
Hanieh Kiaee <hanieh.k@780.ir>
2023-08-30 11:51:05 +0330

    Adding the base repository and its implementation

---
+++ Infrastructure/Data/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Domain.Core;
+using Infrastructure.Data.Service.Paging;
+using Infrastructure.Data.Service.Sorting;
+
+namespace Infrastructure.Data.Repositories.Contracts
+{
+    public interface IRepository<T, key> where T : IEntity<key>
+    {
+        Task<T> GetByIdAsync(key key);
+        IEnumerable<T> GetAll();
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+        Task<PagedList<T>> CustomGet(Expression<Func<T, bool>> expression, string searchText, PagingParam pagingParam = default, SortingParam<T> sorting = default, bool tracking = default);
+    }
+}
---
+++ Infrastructure/Data/Repositories/Core/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Core;
+using Infrastructure.Data.Repositories.Contracts;
+using Infrastructure.Data.Service.Paging;
+using Infrastructure.Data.Service.Sorting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories.Core
+{
+    public class Repository<T, Key> : IRepository<T, Key>
+        where T : Entity<Key>
+    {
+        protected readonly LibraryContext _context;
+        protected readonly DbSet<T> _set;
+        public Repository(LibraryContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _set.AsEnumerable();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Key key)
+        {
+            return await _set.FindAsync(key);
+        }
+
+        public async Task Add(T entity)
+        {
+            await _set.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<PagedList<T>> CustomGet(Expression<Func<T, bool>> expression, string searchText, PagingParam paging, SortingParam<T> sorting, bool tracking = false)
+        {
+            return await _set.Tracking(tracking).
+                Where(expression).
+                Searching(searchText).
+                AsSorting(sorting).
+                PagingAsync(paging);
+        }
+
+        public virtual async Task Remove(T entity)
+        {
+            _set.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _set.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
