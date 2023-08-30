f2a2c2552ffe270ce9c64aa5eb9d22314033f3b4
Hanieh Kiaee <hanieh.k@780.ir>
2023-08-30 11:58:52 +0330

    Migrations are added

---
+++ Infrastructure/Migrations/20220815165924_date.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class date : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Published_Date",
+                schema: "Library",
+                table: "Book",
+                type: "varchar(8)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Published_Date",
+                schema: "Library",
+                table: "Book",
+                type: "varchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldNullable: true);
+        }
+    }
+}
